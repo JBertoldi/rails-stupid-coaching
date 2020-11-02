84fcbd3e55f016026ae499d71de877117309f434
Joana Bertoldi <joanasoares_1994@msn.com>
2020-11-02 12:43:18 +0000

    Added backlink from /answer to /ask

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,12 +4,5 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
-    # if @question == 'If the message is I am going to work'
-    #   'Great!'
-    # elsif @question.include? '?'
-    #   'Silly question, get dressed and go to work!'
-    # else
-    #   'I don\'t care, get dressed and go to work!'
-    # end
   end
 end
